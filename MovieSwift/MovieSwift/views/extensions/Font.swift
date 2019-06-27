fe8d1f482017b765ebb48df2f371d9bb921ae064
dmx <xudianmyahoo@gmail.com>
2019-06-26 23:55:37 -0400

    add comment why the font extension is a static func instead of static var

--- MovieSwift/MovieSwift/views/extensions/Font.swift
+++ MovieSwift/MovieSwift/views/extensions/Font.swift
@@ -9,6 +9,9 @@
 import Foundation
 import SwiftUI
 
+
+//DMX: why the font extension defines func instead of static var?
+//also pay attention here, the func is a static nevertheless
 extension Font {
     public static func FHACondFrenchNC(size: Length) -> Font {
         return Font.custom("FHA Condensed French NC", size: size)
