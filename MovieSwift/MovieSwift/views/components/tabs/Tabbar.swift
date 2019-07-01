73adcffd91e04485336d3b0505571588873194e5
dmx <xudianmyahoo@gmail.com>
2019-06-30 21:19:52 -0400

    added comment on the Tab selection, as well as why use an EnvironmentObject at all

--- MovieSwift/MovieSwift/views/components/tabs/Tabbar.swift
+++ MovieSwift/MovieSwift/views/components/tabs/Tabbar.swift
@@ -9,7 +9,12 @@
 import SwiftUI
 
 struct Tabbar : View {
+    //DMX why use an EnvironmentObject instead of just passed it in
+    //here is the code in SceneDelegate
+    //UIHostingController(rootView: Tabbar().environmentObject(AppStore.global))
+    //How does this store variable gets assigned/or injected?
     @EnvironmentObject var store: AppStore
+    //DMX by default the movies tab is selected
     @State var selectedTab = Tab.movies
     
     enum Tab: Int {
@@ -17,12 +22,13 @@ struct Tabbar : View {
     }
 
     var body: some View {
+        //
         TabbedView(selection: $selectedTab) {
             MoviesHome().tabItemLabel(Text("Movies")).tag(Tab.movies)
             DiscoverView().tabItemLabel(Text("Discover")).tag(Tab.discover)
             MyLists().tabItemLabel(Text("My Lists")).tag(Tab.myLists)
         }
-            .edgesIgnoringSafeArea(.top)
+        .edgesIgnoringSafeArea(.top)
     }
 }
 
